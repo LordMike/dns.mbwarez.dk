afdd1bf8b2a50afaea7992106f9dd3a23277d40c
Michael Bisbjerg <michael@mbwarez.dk>
2016-05-25 16:32:36 +0200

    Removed areas and other unused stuff

--- DnsMbwarezDk/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DnsMbwarezDk.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DnsMbwarezDk/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DnsMbwarezDk.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
