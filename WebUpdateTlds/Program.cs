e4886febd910fbd70e44ead2712ca213393e2588
Michael Bisbjerg <michael@mbwarez.dk>
2016-05-25 18:04:07 +0200

    Updates to process

--- WebUpdateTlds/Program.cs
+++ WebUpdateTlds/Program.cs
@@ -6,8 +6,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Data.Entity;
+using System.Dynamic;
+using System.Runtime.Serialization.Formatters.Binary;
 using DnsLib2.Common;
+using DnsLib2.Enums;
 using DnsLib2.Records;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Shared;
 using WebShared.Db;
 using WebShared.Utilities;
@@ -45,7 +50,7 @@ namespace WebUpdateTlds
 
                 Console.WriteLine("Cancellation requested");
             };
-            
+
             _ravenClient?.CaptureMessage("Began run");
 
             // Config
@@ -53,6 +58,16 @@ namespace WebUpdateTlds
             _nameserverCacheFile = args[1];
             _tldConfigFile = args[2];
             _logFileName = args[3];
+
+            JsonConvert.DefaultSettings = () =>
+            {
+                var settings = new JsonSerializerSettings();
+                //settings.Converters.Add(new IPAddressConverter());
+                //settings.Converters.Add(new FqdnConverter());
+                //settings.Converters.Add(new BaseConverter());
+
+                return settings;
+            };
 
             _logWriter = new StreamWriter(_logFileName);
 
@@ -62,21 +77,22 @@ namespace WebUpdateTlds
 
                 // Load nameserver cache
                 NameserverCache nameserverCache = new NameserverCache(recursiveServer);
-                if (File.Exists(_nameserverCacheFile))
-                    nameserverCache.Load(_nameserverCacheFile);
+                //if (File.Exists(_nameserverCacheFile))
+                //    nameserverCache.Load(_nameserverCacheFile);
 
                 // Load current config
                 TldConfigCollection configs = TldConfigCollection.FromFile(_tldConfigFile);
 
                 // Load current rootzone
                 List<FQDN> roots = FetchRootZone(nameserverCache);
+
                 List<FQDN> secondLevels = roots.Select(configs.Get)
                         .NotNull()
                         .SelectMany(s => s.SecondLevels.Select(x => new FQDN(x)))
                         .ToList();
 
-                Log("Main", "Loaded: " + roots.Count + " roots, " + secondLevels.Count + " second-levels");
-                Log("Main", "Loaded: " + configs.Count + " TLD configs");
+                Log("Main", $"Loaded: {roots.Count:N0} roots, {secondLevels.Count:N0} second-levels");
+                Log("Main", $"Loaded: {configs.Count:N0} TLD configs");
 
                 using (DnsDb db = new DnsDb())
                 {
@@ -100,7 +116,7 @@ namespace WebUpdateTlds
 
                             db.Domains.Add(domainItem);
 
-                            Log("Main", "Adding " + fqdn.Name);
+                            Log("Main", $"Adding {fqdn.Name}");
                         }
 
                         // Update domain
@@ -128,25 +144,18 @@ namespace WebUpdateTlds
                             {
                                 domainItem.Info.LastUpdateUtc = DateTime.UtcNow;
 
-                                Log("Main", "Updated info for " + fqdn.Name);
+                                Log("Main", $"Updated info for {fqdn.Name}");
                             }
                         }
 
                         // Update servers
-                        List<IPAddress> expectedServers = nameserverCache.GetDomainNameserverIps(fqdn).ToList();
-                        ICollection<TldServer> servers = domainItem.Servers;
-
-                        IEnumerable<TldServer> toRemove = servers.Where(s => !expectedServers.Select(x => x.ToString()).Contains(s.ServerIp));
-                        foreach (TldServer server in toRemove)
-                        {
-                            domainItem.Servers.Remove(server);
-
-                            Log("Main", "Removing server from " + fqdn.Name);
-                        }
+                        Dictionary<string, TldServer> currentServers = domainItem.Servers.ToDictionary(s => s.ServerIp);
 
                         foreach (FQDN nameserver in nameserverCache.GetDomainNameservers(fqdn))
                             foreach (IPAddress address in nameserverCache.GetNameserverIps(nameserver))
                             {
+                                currentServers.Remove(address.ToString());
+
                                 TldServer serverItem = domainItem.Servers.SingleOrDefault(s => s.ServerIp == address.ToString());
                                 if (serverItem == null)
                                 {
@@ -171,12 +180,19 @@ namespace WebUpdateTlds
 
                                     domainItem.Servers.Add(serverItem);
 
-                                    Log("Main", "Adding server to " + fqdn.Name);
+                                    Log("Main", $"Adding server {fqdn.Name} / {serverItem.ServerIp}");
                                 }
 
                                 // Update server
                                 serverItem.ServerName = nameserver.Name;
                             }
+
+                        foreach (var server in currentServers.Values)
+                        {
+                            domainItem.Servers.Remove(server);
+
+                            Log("Main", $"Removing server {fqdn.Name} / {server.ServerIp}");
+                        }
                     }
 
                     Log("Main", "Saving to DB");
@@ -184,7 +200,7 @@ namespace WebUpdateTlds
                 }
 
                 // Save
-                nameserverCache.Save(_nameserverCacheFile);
+                // nameserverCache.Save(_nameserverCacheFile);
 
                 Log("Main", "Finished");
             }
@@ -216,23 +232,29 @@ namespace WebUpdateTlds
             string rootFile = "cache_rootzone.bin";
             string rootServer = "xfr.lax.dns.icann.org";
 
-            if (!File.Exists(rootFile) || new FileInfo(rootFile).LastWriteTimeUtc < DateTime.UtcNow.AddDays(-3))
-            {
-                // Refresh
-                IPAddress rootIp = Dns.GetHostAddresses(rootServer).First(s => s.AddressFamily == AddressFamily.InterNetwork);
-                IPEndPoint rootEndpoint = new IPEndPoint(rootIp, 53);
-                List<DnsRecordView> rootZoneRecords = DnsUtilities.DoAxfr(rootEndpoint, ".");
+            List<DnsRecordView> rootZoneRecords;
+            //if (!File.Exists(rootFile) || new FileInfo(rootFile).LastWriteTimeUtc < DateTime.UtcNow.AddDays(-3))
+            //{
+            // Refresh
+            IPAddress rootIp = Dns.GetHostAddresses(rootServer).First(s => s.AddressFamily == AddressFamily.InterNetwork);
+            IPEndPoint rootEndpoint = new IPEndPoint(rootIp, 53);
+            rootZoneRecords = DnsUtilities.DoAxfr(rootEndpoint, ".");
 
-                foreach (DnsRecordView record in rootZoneRecords)
-                    cache.RecordDnsAnswer(record);
+            //File.WriteAllText(rootFile, JsonConvert.SerializeObject(rootZoneRecords.Select(s => s.)));
+            //SerializeToFile(rootZoneRecords, rootFile);
+            //}
+            //else
+            //{
+            //    //rootZoneRecords = JsonConvert.DeserializeObject<List<DnsRecordView>>(File.ReadAllText(rootFile));
+            //    rootZoneRecords = DeserializeFromFile<List<DnsRecordView>>(rootFile);
+            //}
 
-                List<FQDN> rootTlds = rootZoneRecords.OfType<RecordViewNS>().Select(s => s.QName).Distinct().ToList();
+            foreach (DnsRecordView record in rootZoneRecords)
+                cache.RecordDnsAnswer(record);
 
-                File.WriteAllLines(rootFile, rootTlds.Select(s => s.ToString()));
-                return rootTlds;
-            }
+            List<FQDN> rootTlds = rootZoneRecords.OfType<RecordViewNS>().Select(s => s.QName).Distinct().ToList();
 
-            return File.ReadLines(rootFile).Select(s => new FQDN(s)).ToList();
+            return rootTlds;
         }
 
         private static void Log(string category, string line)
@@ -243,5 +265,100 @@ namespace WebUpdateTlds
             _logWriter.WriteLine(logLine);
             _logWriter.Flush();
         }
+
+        private static TData DeserializeFromFile<TData>(string file)
+        {
+            byte[] b = File.ReadAllBytes(file);
+            using (var stream = new MemoryStream(b))
+            {
+                var formatter = new BinaryFormatter();
+                stream.Seek(0, SeekOrigin.Begin);
+                return (TData)formatter.Deserialize(stream);
+            }
+        }
+
+        private static void SerializeToFile<TData>(TData settings, string file)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, settings);
+                stream.Flush();
+                stream.Position = 0;
+
+                File.WriteAllBytes(file, stream.ToArray());
+            }
+        }
     }
+
+    //public class BaseConverter : JsonConverter
+    //{
+    //    static JsonSerializerSettings SpecifiedSubclassConversion = new JsonSerializerSettings()
+    //    {
+    //        //ContractResolver = new BaseSpecifiedConcreteClassConverter()
+    //    };
+
+    //    public override bool CanConvert(Type objectType)
+    //    {
+    //        return (objectType == typeof(DnsRecordView));
+    //    }
+
+    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    //    {
+    //        JObject jo = JObject.Load(reader);
+    //        Type type = DnsRecordView.GetRecordViewType((QType)jo["QType"].Value<int>());
+
+    //        return JsonConvert.DeserializeObject(jo.ToString(), type);
+    //    }
+
+    //    public override bool CanWrite
+    //    {
+    //        get { return false; }
+    //    }
+
+    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    //    {
+    //        throw new NotImplementedException(); // won't be called because CanWrite returns false
+    //    }
+    //}
+
+    //public class FqdnConverter : JsonConverter
+    //{
+    //    public override bool CanConvert(Type objectType)
+    //    {
+    //        return (objectType == typeof(FQDN));
+    //    }
+
+    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    //    {
+    //        FQDN val = (FQDN)value;
+    //        writer.WriteValue(val.Name);
+    //    }
+
+    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    //    {
+    //        JToken token = JToken.Load(reader);
+    //        return new FQDN(token.Value<string>());
+    //    }
+    //}
+
+    //public class IPAddressConverter : JsonConverter
+    //{
+    //    public override bool CanConvert(Type objectType)
+    //    {
+    //        return (objectType == typeof(IPAddress));
+    //    }
+
+    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    //    {
+    //        IPAddress ip = (IPAddress)value;
+    //        writer.WriteValue(ip.ToString());
+    //    }
+
+    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    //    {
+    //        JToken token = JToken.Load(reader);
+    //        return IPAddress.Parse(token.Value<string>());
+    //    }
+    //}
 }
